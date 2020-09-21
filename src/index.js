b494b2071ff22cf3124ecf3e6468223a2bc9c6b9
Suman Jha <meetsumanat@gmail.com>
2020-09-20 23:38:45 -0700

    implementation using context api

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { combineReducers, createStore } from 'redux';
+// import { Provider } from 'react-redux';
+// import { combineReducers, createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import productReducer from './store/reducers/products';
+// import productReducer from './store/reducers/products';
+import ProductsProvider from './context/products-context';
 
-const rootReducer = combineReducers({
-  shop: productReducer,
-});
+// const rootReducer = combineReducers({
+//   shop: productReducer,
+// });
 
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <ProductsProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
+    </ProductsProvider>
     ,
   </React.StrictMode>,
   document.getElementById('root')
